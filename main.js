03e35745f22e10b691d1166e65bcd42b73435fa6
GuillN <gnaassan@gmail.com>
2020-03-26 17:33:25 +0100

    count words done

---
+++ main.js
@@ -0,0 +1,9 @@
+const countWords = string => {
+    let output = {}
+    const splitString = string.split(" ")
+    const words = splitString.filter(word => word !== "")
+    words.map(word => output[word] === undefined ? output[word] = 1 : output[word] += 1)
+    return output
+}
+
+console.log(countWords('titi toto Tata Titi titi     ti ti'))
